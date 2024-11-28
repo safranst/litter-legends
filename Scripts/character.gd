90e29e373380d28061d7ec7b8f81b3ab69e0d06f
Mashiroo21 <rajendraaditya99@gmail.com>
2024-11-28 20:51:19 +0700

    Update Animation
    Animation jump walk etc
    - character.gd for calling animation from animation player.
    - female_char_example.gd for the physics of player.

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -1,14 +1,46 @@
-extends RigidBody3D
+class_name CharacterEx extends Node3D
 
-var velocity = Vector3(0, 0, 0)
+@onready var animation_player = %AnimationPlayer
+#@onready var state_machine : AnimationNodeStateMachinePlayback = animation_player.get("parameters/StateMachine/playback")
+#@onready var move_tilt_path : String = "parameters/StateMachine/Move/tilt/add_amount"
+
+#var run_tilt = 0.0 : set = _set_run_tilt
+
+#@export var blink = true : set = set_blink
+#@onready var blink_timer = %BlinkTimer
+#@onready var closed_eyes_timer = %ClosedEyesTimer
+#@onready var eye_mat = $sophia/rig/Skeleton3D/Sophia.get("surface_material_override/2")
 
 func _ready():
 	pass
 
-func _physics_process(delta):
-	while (Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left")):
-		if Input.is_action_pressed("ui_right"):
-			velocity.x = 5
-		if Input.is_action_pressed("ui_left"):
-			velocity.x = -5
-	move_and_collide(velocity)
+#func set_blink(state : bool):
+	#if blink == state: return
+	#blink = state
+	#if blink:
+		#blink_timer.start(0.2)
+	#else:
+		#blink_timer.stop()
+		#closed_eyes_timer.stop()
+
+#func _set_run_tilt(value : float):
+	#run_tilt = clamp(value, -1.0, 1.0)
+	#animation_player.set(move_tilt_path, run_tilt)
+
+func idle():
+	animation_player.play("idle")
+
+func move():
+	animation_player.play("walk")
+
+func fall():
+	animation_player.play("fall")
+
+func jump():
+	animation_player.play("jump")
+
+#func edge_grab():
+	#state_machine.travel("EdgeGrab")
+
+#func wall_slide():
+	#state_machine.travel("WallSlide")
--- Scripts/female_char_exmple.gd
+++ Scripts/female_char_exmple.gd
@@ -12,15 +12,19 @@ extends CharacterBody3D
 @export_range(0.0, 1.0) var mouse_sensitivity := 0.25
 
 @export_group("Movement")
-@export var move_speed := 20.0
-@export var acceleration := 10.0
+@export var move_speed := 10.0
+@export var acceleration := 20.0
+#@export var rotation_speed := 8.0
+@export var jump_impulse := 10.0
 
 
 var _camera_input_direction := Vector2.ZERO
 var _last_movement_direction := Vector3.BACK
+var _gravity := -30.0
 
 @onready var _camera_pivot: Node3D = %CameraPivot
 @onready var _camera: Camera3D = %Camera3D
+@onready var _skin: CharacterEx = %CharacterEx
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_click"):
@@ -50,13 +54,36 @@ func _physics_process(delta: float) -> void:
 	move_direction.y = 0.0
 	move_direction = move_direction.normalized()
 	
+	var y_velocity := velocity.y
+	velocity.y = 0.0
+	
 	velocity = velocity.move_toward(move_direction * move_speed, acceleration * delta)
+	velocity.y = y_velocity + _gravity * delta
+	
+	var is_starting_jump := Input.is_action_just_pressed("jump") and is_on_floor()
+	if is_starting_jump:
+		velocity.y += jump_impulse
+	
 	move_and_slide()
 	
-	#if move_direction.length() > 0.2:
-		#_last_movement_direction = move_direction
-	#var target_angle := Vector3.BACK.signed_angle_to(_last_movement_direction, Vector3.UP)
-	#global_rotation.y = target_angle
+	if move_direction.length() > 0.2:
+		_last_movement_direction = move_direction
+	var target_angle := Vector3.BACK.signed_angle_to(_last_movement_direction, Vector3.UP)
+	#_skin.global_rotation.y = lerp_angle(_skin.rotation.y, target_angle, rotation_speed * delta)
+	_skin.global_rotation.y = target_angle
+	
+	if is_starting_jump:
+		_skin.jump()
+	elif not is_on_floor() and velocity.y < 0:
+		_skin.fall()
+	elif is_on_floor():
+		var ground_speed := velocity.length()
+		if ground_speed > 0.0:
+			_skin.move()
+		else:
+			_skin.idle()
+	
+	
 	
 #	Kodingan Lama
 	#var direction = Vector3.ZERO
